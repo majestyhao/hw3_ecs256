512dbbd895017349e66e0e6521b770953b1d8fa5
Hao Fu <jamesfuhao@gmail.com>
2014-02-12 20:05:27 -0800

    created files

---
+++ P2a.R
@@ -0,0 +1,8 @@
+# handle mixture and approximation of a constant randoms variable (single Erlang is used)
+# r is the desired value for the Erlang parameter r
+# nmix is the desired # of compoents in the mixture; for use only if const is NULL
+# qftn is the quantile function for the given distribution
+# const is the value of a constant that we wish to approximate
+erlangmix <- function(r, nmix = NULL, qftn = NULL, const = NULL) {
+  
+}
