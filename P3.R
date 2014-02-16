f4bf87ddfa2edc21a062f4e63dd2a2cdb1706a01
Hao Fu <jamesfuhao@gmail.com>
2014-02-16 10:24:16 -0800

    fix small error

--- P3.R
+++ P3.R
@@ -3,7 +3,7 @@ htof <- function(hftn, t, lower) {
   len <- length(t)
   pdf <- rep(0, len)
   for (i in 1:len) {
-    pdf[i] <- hftn(t[i]) * exp(-1 * integrate(hftn, lower, t[i])$val.)
+    pdf[i] <- hftn(t[i]) * exp(-1 * integrate(hftn, lower, t[i])$val)
   }
   return(pdf)
 }
